4039d23c96ad7b10f790d9a05f70849c32ad7018
johnafariogun <afariogun.john@yahoo.com>
2022-07-23 17:07:56 +0100

    edited the login page

--- pricebeta/apps/main/models.py
+++ pricebeta/apps/main/models.py
@@ -1,6 +1,7 @@
 # importing relevant classes i'm  using User cause it is better than creating a custom user which might be more stressful
 from django.db import models
 from django.contrib.auth.models import User
+
 # Create your models here.
 
 
@@ -8,5 +9,6 @@ class Items(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) # importing the user attributes from User and setting on_delete to cascade to delete all items associated to the user
     url = models.CharField(max_length=700) # creating a url field for the ecommerce apps
     max_price = models.FloatField() # creating a price field so that we can alert the user for price change
+
     
 
--- pricebeta/apps/pages/views.py
+++ pricebeta/apps/pages/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render, redirect 
+from django import forms
+from django.contrib.auth.models import User
+
 # importing shortcuts classes
 from .forms import UserRegisterForm
 # importing the user creation form created
-
+class SignUpForm(UserRegisterForm):
+    first_name = forms.CharField(max_length=30)
+    last_name = forms.CharField(max_length=30)
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2' )
+        
 # Create your views here.
 # Defining the home/landing view
 def home(request):
@@ -11,17 +20,17 @@ def home(request):
 # defining the registration view
 def register(request):
     # first creating an empty form
-    form = UserRegisterForm
+    form = SignUpForm
     # defining what to do when the form has being submitted
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
+        form = SignUpForm(request.POST)
 
         if form.is_valid():
             form.save()
             return redirect('/login')
 
     else:
-        form = UserRegisterForm()
+        form = SignUpForm()
 
     context = {
         'form': form
